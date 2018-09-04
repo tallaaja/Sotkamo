8457bd57b9516ace4042b8748097d8c2c930bcd3
tallaaja <mustonenpasii@gmail.com>
2018-09-04 12:50:57 +0300

    presotkamo commit

--- Assets/Scripts/PluginWrapper.cs
+++ Assets/Scripts/PluginWrapper.cs
@@ -1,17 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PluginWrapper : MonoBehaviour {
+public class PluginWrapper : MonoBehaviour
+{
+    public static string json;
+    public Text jotain;
+    AndroidJavaClass unityClass;
+    AndroidJavaObject unityActivity;
+    AndroidJavaObject unityContext;
+    AndroidJavaClass customClass;
 
-    public Text text;
-	// Use this for initialization
-	void Start () {
-        var plugin = new AndroidJavaClass("com.example.geofencing.PluginClass");
-        text.text = plugin.CallStatic<string>("GetTextFromPlugin", 7);
+    float[] longitude;
+    float[] latitude;
+
+    void Start()
+     {
+         //Replace with your full package name
+         //sendActivityReference("com.example.geofencing.PluginClass");
+
+         //Now, start service
+         //startService();
+     }
+
+
+
+     void sendActivityReference(string packageName)
+     {
+         unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+         unityActivity = unityClass.GetStatic<AndroidJavaObject>("currentActivity");
+         unityContext = unityActivity.Call<AndroidJavaObject>("getApplicationContext");
+
+         customClass = new AndroidJavaClass(packageName);
+         customClass.CallStatic("receiveContextInstance", unityContext);
+     }
+
+
+     void startService()
+     {
+        JsonObject myObject = new JsonObject();
+        myObject.longitude = 24.3f;
+        myObject.latitude = 22.2f;
+        json = JsonUtility.ToJson(myObject);
+        Debug.Log(json);
+        jotain.text = customClass.CallStatic<string>("StartCheckerService", json);
+
+     }
+
+    [Serializable]
+    private class JsonObject
+    {
+        public float longitude;
+        public float latitude;
 
 
     }
 
 }
+
---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour {
+
+    public Button FixedRoute;
+    public Button MakeYourOwnRoute;
+
+	// Use this for initialization
+	void Start () {
+
+        FixedRoute.onClick.AddListener(FixedRouteScene);
+        MakeYourOwnRoute.onClick.AddListener(MakeYourOwnRouteScene);
+
+    }
+
+    void FixedRouteScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void MakeYourOwnRouteScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
